5d95b6a231e83d26bf0cdf5afc42fe4a3ca3b268
eyecmsl <idkwhattosay626@gmail.com>
2026-05-26 19:00:54 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,10 +2,8 @@ local R = "https://raw.githubusercontent.com/eyecmsl/soda/main/"
 local M = loadstring(game:HttpGet(R .. "manifest.lua"))()
 local id = tostring(game.PlaceId)
 print("soda main " .. M.main .. " | game " .. id .. " v" .. (M.games[id] or "?"))
-local function loadGame(p)
-	local s, e = pcall(game.HttpGet, game, R .. "games/" .. p .. ".lua")
-	assert(s, e)
-	loadstring(e)()
+local function get(u)
+	return game:HttpGet(R .. "games/" .. u .. ".lua")
 end
-local ok, _ = pcall(loadGame, id)
-if not ok then loadGame("_Universal") end
+local ok, e = pcall(get, id)
+loadstring((ok and e or get("_Universal")))()
